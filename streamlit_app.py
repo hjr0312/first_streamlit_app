cabe50773cef091e40f5b34ffe9e47a4f7685aff
hjr0312 <106720527+hjr0312@users.noreply.github.com>
2022-06-01 21:54:16 -0600

    Update streamlit_app.py
    adding panda and wireframe

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,4 +3,6 @@ streamlit.title('i love nubby')
 streamlit.header('things i love about nubby:')
 streamlit.text('hugs')
 streamlit.text('kisses')
-
+import pandas
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
